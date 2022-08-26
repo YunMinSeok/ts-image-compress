bf050d0e69c1d50a1e8e87cebc7a61a7629a0121
윤민석 <ykmvm147@naver.com>
2022-08-26 21:59:57 +0900

    feat: update ts,package json file && create dist,lib folder

---
+++ dist/imageCompress.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var imageCompress = /** @class */ (function () {
+    function imageCompress() {
+    }
+    return imageCompress;
+}());
+exports.default = imageCompress;
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ImageCompress = void 0;
+var imageCompress_1 = require("./imageCompress");
+Object.defineProperty(exports, "ImageCompress", { enumerable: true, get: function () { return imageCompress_1.default; } });
