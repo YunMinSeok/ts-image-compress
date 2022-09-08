058d4652d2596a004f7ac41c855283ef7669e306
윤민석 <ykmvm147@naver.com>
2022-09-08 13:38:51 +0900

    feat: imgcompress code update && test file update

--- src/imageCompress.tsx
+++ src/imageCompress.tsx
@@ -1,5 +1,5 @@
 interface fileType {
-  imgFile: File | FileList;
+  imgFile: File;
 }
 
 const imageCompress = async (imageFile: fileType) => {
@@ -7,29 +7,25 @@ const imageCompress = async (imageFile: fileType) => {
 
   const image = document.createElement("img");
   const resizeImage = await new Promise(async (resolve) => {
-    console.log(imageFile);
-    for (let cnt = 0; cnt < imageFile.imgFile.length; cnt++) {
-      const file = imageFile[cnt];
-      image.src = URL.createObjectURL(file);
-      image.onload = async () => {
-        await URL.revokeObjectURL(image.src);
-        const canvas = document.createElement("canvas");
-        const { width, height } = calcTargetSize(image.width, image.height);
-        canvas.width = width;
-        canvas.height = height;
-        const context = canvas.getContext("2d");
-        context?.drawImage(image, 0, 0, width, height);
-        context?.canvas.toBlob(
-          (newImageBlob) => {
-            if (newImageBlob) {
-              resolve(new File([newImageBlob], file.name));
-            }
-          },
-          file.type,
-          customQuality
-        );
-      };
-    }
+    image.src = URL.createObjectURL(imageFile.imgFile);
+    image.onload = async () => {
+      await URL.revokeObjectURL(image.src);
+      const canvas = document.createElement("canvas");
+      const { width, height } = calcTargetSize(image.width, image.height);
+      canvas.width = width;
+      canvas.height = height;
+      const context = canvas.getContext("2d");
+      context?.drawImage(image, 0, 0, width, height);
+      context?.canvas.toBlob(
+        (newImageBlob) => {
+          if (newImageBlob) {
+            resolve(new File([newImageBlob], imageFile.imgFile.name));
+          }
+        },
+        imageFile.imgFile.type,
+        customQuality
+      );
+    };
   });
   return resizeImage;
 };
